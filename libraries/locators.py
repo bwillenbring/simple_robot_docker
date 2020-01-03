d1e3c55b882694ccde0d2cf30bc1e430dcd1b378
Benjamin Willenbring <benjamin.willenbring@autodesk.com>
2020-01-03 16:45:06 -0500

    Adding locators

--- libraries/locators.py
+++ libraries/locators.py
@@ -17,3 +17,18 @@ class LoginFormLocators(object):
 class MainPageLocators(object):
     # Shotgun global header
     HEADER = (By.ID, 'sg_global_nav')
+
+    # Spinners
+    SPINNER = (By.CSS_SELECTOR, '[data_cy="overlay-spinner"]')
+
+    # TOOLBAR => Page Mode Buttons
+    PAGEMODE_BUTTON_BROWSER = (By.CSS_SELECTOR, '[sg_selector="button:mode_browser"]')
+    PAGEMODE_BUTTON_LIST = (By.CSS_SELECTOR, '[sg_selector="button:mode_list"]')
+    PAGEMODE_BUTTON_THUMB = (By.CSS_SELECTOR, '[sg_selector="button:mode_thumb"]')
+    PAGEMODE_BUTTON_CARD = (By.CSS_SELECTOR, '[sg_selector="button:mode_card"]')
+    PAGEMODE_BUTTON_CALENDAR = (By.CSS_SELECTOR, '[sg_selector="button:mode_calendar"]')
+
+    # TOOLBAR => Quickfilter
+    QUICK_FILTER_JUMP_MENU = (By.CSS_SELECTOR, '.quick_filter .quick_jump_menu.icon_disclosure')
+    QUICK_FILTER_TEXT_INPUT = (By.CSS_SELECTOR, '[sg_selector="input:quick_filter"]')
+    QUICK_FILTER_CLEAR_BTN = (By.CSS_SELECTOR, '[sg_selector="button:clear_filter"]')
