db985e8b970df09a6eea9c67e87cd928c13c3f8b
Benjamin Willenbring <benjamin.willenbring@autodesk.com>
2020-01-05 23:00:01 -0500

    Adding 2 more simple specs to test Mocha html reports

---
+++ cypress/integration/simple-spec2.js
@@ -0,0 +1,11 @@
+describe('Sample Spec 2', function() {
+    it('adds 4+4', function() {
+        assert.isTrue(4+4===8);
+        assert.isTrue(0 < 1);
+        /**
+         # Here are extended comments (for testing of Mocha Reports)
+         - ones
+         - two
+        */
+    })
+})
