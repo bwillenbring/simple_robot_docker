8a0bf9524a4ceb8fa94963ad11ff57ba21eaf1c2
Benjamin Willenbring <benjamin.willenbring@autodesk.com>
2020-01-06 16:48:19 -0500

    Cutting down simple spec

--- cypress/integration/simple-spec.js
+++ cypress/integration/simple-spec.js
@@ -10,25 +10,25 @@ describe('Sample Spec', function() {
         })
     });
 
-    it('logs in as admin', function() {
-        cy.login_admin();
-    })
-
-    it('goes to configured home page', function() {
-        cy.home();
-        cy.url().should('contain', Cypress.config('baseUrl'));
-    })
-
-    it('navigate to page 882', function() {
-        cy.navigate_to_page('882'); // page should be a string like '/page/account_settings'
-    })
-
-    it('navigate to Project Page Asset', function() {
-        cy.navigate_to_project_page('Asset'); // page should be a string like '/page/account_settings'
-    })
-
-    it('navigates to page xyz and gets Page not Found', function() {
-        cy.navigate_to_page('/page/xyz'); // page should be a string like '/page/account_settings'
-        cy.get('body').should('contain', "Sorry, we can't find this page")
-    })
+    // it('logs in as admin', function() {
+    //     cy.login_admin();
+    // })
+    //
+    // it('goes to configured home page', function() {
+    //     cy.home();
+    //     cy.url().should('contain', Cypress.config('baseUrl'));
+    // })
+    //
+    // it('navigate to page 882', function() {
+    //     cy.navigate_to_page('882'); // page should be a string like '/page/account_settings'
+    // })
+    //
+    // it('navigate to Project Page Asset', function() {
+    //     cy.navigate_to_project_page('Asset'); // page should be a string like '/page/account_settings'
+    // })
+    //
+    // it('navigates to page xyz and gets Page not Found', function() {
+    //     cy.navigate_to_page('/page/xyz'); // page should be a string like '/page/account_settings'
+    //     cy.get('body').should('contain', "Sorry, we can't find this page")
+    // })
 })
