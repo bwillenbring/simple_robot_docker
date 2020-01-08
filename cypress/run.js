cffda7a867b9077242c3dee399ce3e631116965e
Benjamin Willenbring <benjamin.willenbring@autodesk.com>
2020-01-08 12:32:38 -0500

    Cleanup and better comments

--- cypress/run.js
+++ cypress/run.js
@@ -2,14 +2,24 @@ const cypress = require('cypress')
 const marge = require('mochawesome-report-generator')
 const { merge } = require('mochawesome-merge')
 
-// See Options below
-const options = {
-  files: ['./reports/*.json'],
-}
+// Run the cypress specs
+cypress.run({
+    spec: './integration/simple*.js'
+}).then((results) => {
+    // Print out the results of the run...
+    console.log(results)
 
-cypress.run().then(
-  () => {
-    generateReport(options)
+    // Run mochawesome-merge on all .json files using these options
+    const options = {
+      files: ['./reports/*.json'],
+    }
+    generateReport(options);
+
+    // If any test fails, send an exit code of 1 so CI knows there is a problem
+    if (results.totalFailed > 0) {
+        console.log('Exiting with code 1 because of failed cypress spec.');
+        process.exit(1);
+    }
   },
   error => {
     generateReport()
@@ -18,6 +28,8 @@ cypress.run().then(
   }
 )
 
+// This is the function merges several json files into 1 json file
+// Then, creates an html report from that 1 json file
 function generateReport(options) {
   return merge(options).then(report => marge.create(report, options));
 }
