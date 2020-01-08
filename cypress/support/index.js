4cc04632936edc61cd5ace554392370b5621f5c6
Benjamin Willenbring <benjamin.willenbring@autodesk.com>
2020-01-07 20:26:35 -0500

    Simplifying main index file

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -3,15 +3,6 @@ import 'cypress-plugin-retries';
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-// import './commands.crud';
-// import './commands.entity_query_page';
-// import './commands.routes';
-// import './commands.login';
-// import './commands.navigation';
-// import './commands.qb';
-// import './commands.file_uploads';
-
-
 
 Cypress.on('uncaught:exception', (err, runnable) => {
     // yields the error (Object), mocha runnable (Object)
