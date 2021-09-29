9348d161236f63b283ae9f7e93c21d6a714b1538
Benjamin Willenbring <bwillenbring@gmail.com>
2021-09-28 21:03:20 -0400

    Update run.js
    Prettify file

--- cypress/run.js
+++ cypress/run.js
@@ -1,31 +1,33 @@
 const cypress = require('cypress')
 const marge = require('mochawesome-report-generator')
 const { merge } = require('mochawesome-merge')
-const moment = require('moment');
+const moment = require('moment')
 
 // See Options below
 const options = {
-  files: ['./reports/*.json'],
-  reportTitle: 'Cypress report'
+    files: ['./reports/*.json'],
+    reportTitle: 'Cypress report',
 }
 
 cypress.run().then(
-  (o) => {
-    // Get the timestamp of the cypress run's completion 
-    let ts = moment(o.endedTestsAt);
-    // Format the ts like... Tue. Sept 28, 2021
-    let tss = `${ts.format('ddd')}. ${moment.monthsShort()[ts.format('M') - 1]} ${ts.format('D')}, ${ts.format('YYYY')} ${ts.format('h:mm a Z utc')}`;
+    (o) => {
+        // Get the timestamp of the cypress run's completion
+        let ts = moment(o.endedTestsAt)
+        // Format the ts like... Tue. Sept 28, 2021
+        let tss = `${ts.format('ddd')}. ${
+            moment.monthsShort()[ts.format('M') - 1]
+        } ${ts.format('D')}, ${ts.format('YYYY')} ${ts.format('h:mm a Z utc')}`
 
-    options.reportTitle = `${tss}`
-    generateReport(options)
-  },
-  error => {
-    generateReport()
-    console.error(error)
-    process.exit(1)
-  }
+        options.reportTitle = `${tss}`
+        generateReport(options)
+    },
+    (error) => {
+        generateReport()
+        console.error(error)
+        process.exit(1)
+    }
 )
 
 function generateReport(options) {
-  return merge(options).then(report => marge.create(report, options));
+    return merge(options).then((report) => marge.create(report, options))
 }
