2e0504ba1d629efb34cea6402ca104a71c470d9e
Benjamin Willenbring <bwillenbring@gmail.com>
2021-09-28 21:04:24 -0400

    Update simple-spec3.js
    Replace placeholder specs with ones that do something

--- cypress/integration/simple-spec3.js
+++ cypress/integration/simple-spec3.js
@@ -1,5 +1,34 @@
-describe('Sample Spec 3', function() {
-    it('adds 2+2', function() {
-        assert.isTrue(2+2===4)
+/// <reference types="cypress" />
+describe('Simple web site spec', function () {
+    it('checks home page for doodler', function () {
+        cy.visit('')
+        // assert that the body contains the text pattern you
+        cy.get('body').contains(/doodler/i)
+        // inline the video recording of the spec in the mochawesome-report
+        cy.addTestContext('Video', `./videos/${Cypress.spec.name}.mp4`)
+    })
+
+    it('generates an ai story', function () {
+        cy.visit('/willenblog/animatronic-ai-story-generator')
+        // assert the story container text is empty
+        cy.get('#story_container')
+            .invoke('text')
+            .invoke('trim')
+            .should('eq', '')
+        // click the get story button
+        cy.get('a[data-initialized]:contains("Get Story")').click()
+        // ensure the story contains 100 characters or more of text
+        cy.waitUntil(() =>
+            cy
+                .get('#story_container')
+                .then((story) => Cypress.$(story).text().length > 100)
+        )
+        // Screenshot the story
+        cy.get('#story_container').screenshot('ai_story')
+        // inline the image of the story
+        cy.addTestContext(
+            'AI Story',
+            `./screenshots/${Cypress.spec.name}/ai_story.png`
+        )
     })
 })
